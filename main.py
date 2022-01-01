05d8b6b331c907477f9d080b66466f7578d14bd5
Oleg Sivakov <Meng@siams.com>
2022-01-01 15:39:45 +0500

    init commit

---
+++ main.py
@@ -0,0 +1,24 @@
+def function_1(x): return x ** 2
+
+
+def function_2(x): return x ** 3
+
+
+def function_3(x): return x ** 4
+
+
+def runCallbacks():
+    callbacks = [function_1, function_2, function_3]
+    print('nNamed Functions: ')
+    for function in callbacks: print('Result:', function(3))
+    callbacks = [lambda x: x ** 2, lambda x: x ** 3, lambda x: x ** 4]
+    dict = {"key1": "value"}
+    for functions in callbacks: print('Result:', function(3))
+    for key in dict.values(): print(key)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    runCallbacks()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
